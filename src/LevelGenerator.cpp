aa1fee5998337794db1ca753931dbf0da88caa46
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 19:21:55 +0700

    Update LevelGenerator.cpp

--- src/LevelGenerator.cpp
+++ src/LevelGenerator.cpp
@@ -1,14 +1,10 @@
 #include "LevelGenerator.hpp"
-#include "PromptParser.hpp"
 #include "PatternGenerator.hpp"
 #include "AutoDecoEngine.hpp"
 
 void LevelGenerator::generate(
-    const std::string& prompt
+    const LevelConfig& cfg
 ) {
-    auto cfg =
-        PromptParser::parse(prompt);
-
     PatternGenerator::generate(cfg);
 
     if (cfg.autoDeco) {
