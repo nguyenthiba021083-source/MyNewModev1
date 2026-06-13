c70d708495e13f082c7394eb46faa259b1148f5d
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 17:51:54 +0700

    Create PatternGenerator.cpp

---
+++ src/PatternGenerator.cpp
@@ -0,0 +1,17 @@
+#include "PatternGenerator.hpp"
+
+void PatternGenerator::generate(
+    const LevelConfig& cfg
+) {
+    switch (cfg.difficulty) {
+
+        case Difficulty::Easy:
+            break;
+
+        case Difficulty::Demon:
+            break;
+
+        default:
+            break;
+    }
+}
