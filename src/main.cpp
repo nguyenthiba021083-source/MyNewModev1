c6fb811f8733b11cca226a7a8a25866281858f92
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 14:02:23 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,10 @@ using namespace geode::prelude;
 
 $execute {
     log::info("AI Level Generator Pro Loaded");
-}
+
+    FLAlertLayer::create(
+        "AI Debug",
+        "Mod Loaded",
+        "OK"
+    )->show();
+    }
