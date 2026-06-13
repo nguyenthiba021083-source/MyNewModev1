842662549c4f0b096551dd2f6ccd71547f974b42
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 18:43:00 +0700

    Create AutoDecoEngine.cpp

---
+++ src/AutoDecoEngine.cpp
@@ -0,0 +1,64 @@
+#include "AutoDecoEngine.hpp"
+#include "LevelConfig.hpp"
+#include "ObjectDatabase.hpp"
+#include "ThemeSystem.hpp"
+#include "EditorLayerBridge.hpp"
+
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+void AutoDecoEngine::decorate(
+    const LevelConfig& cfg
+) {
+    auto editor = EditorLayerBridge::editor;
+
+    if (!editor) {
+        log::error(
+            "AutoDecoEngine: Editor not found"
+        );
+        return;
+    }
+
+    log::info(
+        "AutoDecoEngine started"
+    );
+
+    log::info(
+        "Theme = {}",
+        cfg.theme
+    );
+
+    //
+    // Future theme decoration system
+    //
+
+    if (cfg.theme == "default") {
+        log::info(
+            "Using default theme"
+        );
+    }
+    else if (cfg.theme == "modern") {
+        log::info(
+            "Using modern theme"
+        );
+    }
+    else if (cfg.theme == "hell") {
+        log::info(
+            "Using hell theme"
+        );
+    }
+    else if (cfg.theme == "space") {
+        log::info(
+            "Using space theme"
+        );
+    }
+
+    //
+    // Future automatic decoration
+    //
+
+    log::info(
+        "AutoDecoEngine finished"
+    );
+}
