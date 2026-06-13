5571c4d60c2740e54043c74a0262bc4c600fe2e0
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 19:22:31 +0700

    Update AISystem.cpp

--- src/AISystem.cpp
+++ src/AISystem.cpp
@@ -1,10 +1,12 @@
 #include "AISystem.hpp"
 #include "LevelGenerator.hpp"
+#include "PromptParser.hpp"
 
 void AISystem::generate(
     const std::string& prompt
 ) {
-    LevelConfig cfg;
+    LevelConfig cfg =
+        PromptParser::parse(prompt);
 
     LevelGenerator::generate(cfg);
 }
