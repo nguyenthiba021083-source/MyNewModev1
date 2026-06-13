ae9dbacc864eb2daca348f3ac27d464835e749db
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 14:42:12 +0700

    Update AIMenu.hpp

--- src/AIMenu.hpp
+++ src/AIMenu.hpp
@@ -1,18 +1,6 @@
 #pragma once
 
-#include <Geode/Geode.hpp>
-#include <Geode/ui/TextInput.hpp>
-
-using namespace geode::prelude;
-
-class AIMenu : public Popup<> {
-protected:
-    TextInput* m_input;
-
-    bool setup() override;
-
-    void onGenerate(CCObject*);
-
+class AIMenu {
 public:
     static void open();
 };
