dcd77795a6bba5d2cb0944fff186d0ef37781366
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 14:43:17 +0700

    Update AIMenu.cpp

--- src/AIMenu.cpp
+++ src/AIMenu.cpp
@@ -1,53 +1,16 @@
 #include "AIMenu.hpp"
 #include "AISystem.hpp"
 
+#include <Geode/Geode.hpp>
+
 using namespace geode::prelude;
 
-bool AIMenu::setup() {
-    this->setTitle("AI Generator");
-
-    m_input = TextInput::create(220.f, "Enter command...");
-    m_input->setPosition({ 0.f, 20.f });
-
-    m_mainLayer->addChild(m_input);
-
-    auto spr = ButtonSprite::create(
-        "Generate",
-        "goldFont.fnt",
-        "GJ_button_01.png",
-        0.8f
-    );
-
-    auto btn = CCMenuItemSpriteExtra::create(
-        spr,
-        this,
-        menu_selector(AIMenu::onGenerate)
-    );
-
-    btn->setPosition({ 0.f, -30.f });
-
-    auto menu = CCMenu::create();
-    menu->setPosition({ 0.f, 0.f });
-
-    menu->addChild(btn);
-
-    m_mainLayer->addChild(menu);
-
-    return true;
-}
-
-void AIMenu::onGenerate(CCObject*) {
-    std::string prompt = m_input->getString();
-
-    AISystem::generate(prompt);
-
+void AIMenu::open() {
     FLAlertLayer::create(
-        "AI",
-        ("Generated: " + prompt).c_str(),
+        "AI Generator",
+        "AI Menu Loaded!",
         "OK"
     )->show();
-}
 
-void AIMenu::open() {
-    AIMenu::create()->show();
+    AISystem::generate("spike");
 }
