763c4b8b255410366e720831b2f7f2929e237f15
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 17:50:43 +0700

    Create ObjectDatabase.hpp

---
+++ src/ObjectDatabase.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace ObjectDB {
+
+    constexpr int SPIKE = 1;
+    constexpr int BLOCK = 2;
+
+    constexpr int GLOW = 743;
+
+    constexpr int YELLOW_ORB = 36;
+    constexpr int BLUE_ORB = 84;
+
+    constexpr int SHIP_PORTAL = 13;
+    constexpr int WAVE_PORTAL = 660;
+}
