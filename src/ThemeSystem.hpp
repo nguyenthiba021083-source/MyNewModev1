336b9e94697fb84cff9d7a6603dc83eb2589548e
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 17:50:02 +0700

    Create ThemeSystem.hpp

---
+++ src/ThemeSystem.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "LevelConfig.hpp"
+
+class ThemeSystem {
+public:
+    static int getBlockID(
+        Theme theme
+    );
+
+    static int getGlowID(
+        Theme theme
+    );
+};
