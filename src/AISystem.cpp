142241c3b46c4756b709651ecb14ea37fa1ffb58
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 19:20:02 +0700

    Update AISystem.cpp

--- src/AISystem.cpp
+++ src/AISystem.cpp
@@ -1,49 +1,10 @@
 #include "AISystem.hpp"
-#include "EditorLayerBridge.hpp"
+#include "LevelGenerator.hpp"
 
-#include <Geode/Geode.hpp>
+void AISystem::generate(
+    const std::string& prompt
+) {
+    LevelConfig cfg;
 
-using namespace geode::prelude;
-
-void AISystem::generate(const std::string& prompt) {
-    auto editor = EditorLayerBridge::editor;
-
-    if (!editor) {
-        log::error("EditorLayer not found");
-        return;
-    }
-
-    if (prompt == "spike") {
-        auto obj = editor->createObject(
-            1,
-            cocos2d::CCPoint(100.f, 100.f),
-            false
-        );
-
-        if (obj) {
-            log::info("Spike created");
-        }
-
-        return;
-    }
-
-    if (prompt == "10 spikes") {
-        for (int i = 0; i < 10; i++) {
-            auto obj = editor->createObject(
-                1,
-                cocos2d::CCPoint(
-                    100.f + i * 30.f,
-                    100.f
-                ),
-                false
-            );
-
-            if (obj) {
-                log::info("Spike {}", i);
-            }
-        }
-
-        log::info("10 spikes created");
-        return;
-    }
+    LevelGenerator::generate(cfg);
 }
