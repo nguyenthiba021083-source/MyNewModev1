91678275b23363ca04232ac22285d4b3ce241910
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-13 14:05:27 +0700

    Update EditorUIHook.cpp

--- src/EditorUIHook.cpp
+++ src/EditorUIHook.cpp
@@ -12,47 +12,22 @@ public:
         if (!EditorUI::init(editorLayer))
             return false;
 
-        // Debug popup để kiểm tra hook có chạy không
+        EditorLayerBridge::editor = editorLayer;
+
         FLAlertLayer::create(
             "AI Debug",
             "EditorUI Hook Loaded",
             "OK"
         )->show();
 
-        // Lưu EditorLayer cho AI System
-        EditorLayerBridge::editor = editorLayer;
-
         auto sprite = CCSprite::createWithSpriteFrameName(
             "GJ_plusBtn_001.png"
         );
 
-        if (!sprite) {
-            log::error("Failed to create AI button sprite");
+        if (!sprite)
             return true;
-        }
 
         auto btn = CCMenuItemSpriteExtra::create(
             sprite,
             this,
-            menu_selector(EditorUIHook::onAIButton)
-        );
-
-        btn->setPosition(ccp(-160.f, 90.f));
-
-        auto menu = this->getChildByType<CCMenu>(0);
-
-        if (menu) {
-            menu->addChild(btn);
-            log::info("AI Button Added");
-        }
-        else {
-            log::error("Could not find CCMenu");
-        }
-
-        return true;
-    }
-
-    void onAIButton(CCObject*) {
-        AIMenu::open();
-    }
-};
+            menu_selector(Editor
