ce870002f15a0329b7ecd9b20d9b0cd94ef4e919
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-14 09:38:37 +0700

    Update EditorLayerBridge.hpp

--- src/EditorLayerBridge.hpp
+++ src/EditorLayerBridge.hpp
@@ -3,6 +3,8 @@
 #include <Geode/Geode.hpp>
 #include <Geode/binding/LevelEditorLayer.hpp>
 
+using namespace geode::prelude;
+
 class EditorLayerBridge {
 public:
     static LevelEditorLayer* editor;
