eeb87965cb552523b8c9bdd074f6675300164800
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-14 10:58:53 +0700

    Update MusicAnalyzer.hpp

--- src/MusicAnalyzer.hpp
+++ src/MusicAnalyzer.hpp
@@ -1,16 +1,15 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 struct MusicSection {
     float start;
     float end;
-
     std::string type;
 };
 
 class MusicAnalyzer {
 public:
-    static std::vector<MusicSection>
-    analyze(float bpm);
+    static std::vector<MusicSection> analyze(float bpm);
 };
