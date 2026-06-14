1c44281d7a6889d8335f52db9e1b5e768ec379a2
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-14 09:32:39 +0700

    Update EditorLayerBridge.cpp

--- src/EditorLayerBridge.cpp
+++ src/EditorLayerBridge.cpp
@@ -1,3 +1,21 @@
 #include "EditorLayerBridge.hpp"
 
-LevelEditorLayer* EditorLayerBridge::editor = nullptr;
+using namespace geode::prelude;
+
+void LevelGenerator::generate(
+    const LevelConfig& cfg
+) {
+    auto editor = EditorLayerBridge::editor;
+
+    if (!editor)
+        return;
+
+    auto obj = editor->createObject(1, {100.f, 100.f}, false);
+
+    if (!obj)
+        return;
+
+    editor->addObject(obj);
+
+    editor->updateVisibility();
+}
