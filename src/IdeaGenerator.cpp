11e10893b30d0785a43b69dca760e6c4c3678181
nguyenthiba021083-source <nguyenthiba021083@gmail.com>
2026-06-14 11:19:17 +0700

    Update IdeaGenerator.cpp

--- src/IdeaGenerator.cpp
+++ src/IdeaGenerator.cpp
@@ -1,17 +1,11 @@
 #include "IdeaGenerator.hpp"
 
 std::string IdeaGenerator::generate(const std::string& prompt) {
-    if (prompt == "hell") {
-        return "Hell theme";
-    }
-
-    if (prompt == "space") {
+    if (prompt == "space")
         return "Space theme";
-    }
 
-    if (prompt == "water") {
-        return "Water theme";
-    }
+    if (prompt == "hell")
+        return "Hell theme";
 
     return "Default level";
 }
